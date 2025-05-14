743960a46bf0da715e374b68a888eed00292317a
isma <ismaiwani29@gmail.com>
2025-05-14 13:29:16 +0800

    ism login button

--- web page.php
+++ web page.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <link rel="stylesheet" href="webPage.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>MyPetakom</title>
- 
-</head>
-<body>
-  <header>
-    <div class="logos">
-      <img src="umpsa-logo.png" alt="UMPSA Logo" />
-      <img src="petakom-logo.png" alt="PETAKOM Logo" />
-    </div>
-    <div class="title">MyPetakom</div>
-  </header>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="webPage.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>MyPetakom</title>
+  
+  </head>
 
-  <main>
-    <h1>Welcome to MyPetakom </h1>
-    <p>Please login to continue</p>
-    <a href="login.html">
-      <button class="login-button">Login</button>
-    </a>
-  </main>
+  <body>
 
-  <footer>
-    &copy; 2025 MyPetakom | Universiti Malaysia Pahang Al-Sultan Abdullah
-  </footer>
-</body>
+    <header>
+
+      <div class="logos">
+        <img src="umpsa-logo.png" alt="UMPSA Logo" />
+        <img src="petakom-logo.png" alt="PETAKOM Logo" />
+      </div>
+      <div class="title">MyPetakom</div>
+
+    </header>
+
+    <main>
+
+      <h1>Welcome to MyPetakom </h1>
+      <p>Please login to continue</p>
+      <a href="login.php" class="login-button">Login</a>
+    
+    </main>
+
+    <footer>
+      &copy; 2025 MyPetakom | Universiti Malaysia Pahang Al-Sultan Abdullah
+    </footer>
+  </body>
 </html>
 
                 
